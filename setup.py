0c3c0b6e126e95ad5905768d94b4e8ae4ccf1f03
Your Name <onlytojay@gmail.com>
2019-02-12 22:53:24 +0900

    version up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name                = 'ypcc',
-    version             = '0.1',
+    version             = '0.2',
     description         = 'ccpy',
     author              = 'jeakwon',
     author_email        = 'onlytojay@gmail.com',
