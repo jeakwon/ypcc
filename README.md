ee1d13bd6ab9fb01d7a4926583cabbcf9645a9b7
Your Name <onlytojay@gmail.com>
2019-02-12 18:22:23 +0900

    setup.py & setup.cfg 추가

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ccpy
 ccpy
+
+# 배포 가능한 파이썬 패키지 만들어보기
+
+참고 링크 :  
+https://rampart81.github.io/post/python_package_publish/
+https://setuptools.readthedocs.io/en/latest/setuptools.html
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name                = 'ccpy',
+    version             = '0.1',
+    description         = 'ccpy',
+    author              = 'jeakwon',
+    author_email        = 'onlytojay@gmail.com',
+    url                 = 'https://github.com/jeakwon/ccpy',
+    download_url        = 'https://github.com/jeakwon/ccpy/archive/0.0.tar.gz',
+    install_requires    =  [],
+    packages            = find_packages(exclude = []),
+    keywords            = ['ccpy'],
+    python_requires     = ['>=3'],
+    package_data        = {},
+    zip_safe            = False,
+    classifiers         = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
