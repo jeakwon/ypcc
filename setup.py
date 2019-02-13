7d892852ecb10af6eac10d565e8f41f1c05b0783
Your Name <onlytojay@gmail.com>
2019-02-13 13:03:31 +0900

    version = '0.3' --> '0.4'

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name                = 'ypcc',
-    version             = '0.3',
+    version             = '0.4',
     description         = 'ccpy',
     license             = 'MIT',
     author              = 'jeakwon',
