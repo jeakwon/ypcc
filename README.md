aae1af26b9850984c6e46a211057c0754216a733
Your Name <onlytojay@gmail.com>
2019-02-14 14:45:23 +0900

    gitignore에 vscode폴더 무시 추가

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 
 # New in v0.5
 https://github.com/jeakwon/ypcc 로 깃허브 주소 변경
-main.py 추가
+array.py 추가
+requiremnts.txt 추가
+.gitignore에 .vscode/ 무시추가 
+
 
 # 배포 가능한 파이썬 패키지 만들어보기
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name                = 'ypcc',
-    version             = '0.4',
+    version             = '0.5',
     description         = 'ccpy',
     license             = 'MIT',
     author              = 'jeakwon',
